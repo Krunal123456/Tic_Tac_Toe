6540d81e0ee81bdb6eb737d4015c6139af81068b
Krunal <krunalkishortote@gmail.com>
2021-04-30 13:51:14 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Square = (props) => {
+  const [value, setValue] = useState(null);
+
+
+  return (
+    <button 
+    className="square"
+    onClick={props.onClickEvent}
+    >
+      {props.value}
+    </button>
+  );
+};
+
+const Board = () => {
+  const initialSquares = Array(9).fill(null);
+ const[squares, setSquares] = useState(initialSquares);
+ const [xIsNext,setXIsNext] = useState(true);
+
+const handleClickEvent =(i) => {
+  const newSquares =[...squares];
+
+   const winnerDeclared = Boolean(calculateWinner(newSquares));
+   const squareFilled = Boolean(newSquares[i]);
+   if (winnerDeclared || squareFilled) {
+     return ;
+   }
+
+  newSquares[i]=xIsNext ? 'X':'O';
+  setSquares(newSquares);
+  setXIsNext(!xIsNext);
+};
+
+
+  const renderSquare = (i) => {
+    return (
+      <Square 
+          value={squares[i]} 
+          onClickEvent={() => handleClickEvent(i)}
+          />
+    );
+  };
+  const winner = calculateWinner(squares);
+  const status = winner ? `Winner:${winner}` : `Next Player: ${xIsNext ? 'X' :'O'}`;
+  
+  
+
+
+
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20,
+    }}>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}      </div>
+      <div className="board-row">
+        {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}     </div>
+      <div className="board-row">
+        {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}      </div>
+    </div>
+  );
+};
+
+
+
+const Game = () => {
+  return (
+    <div className="game">
+      Tic_Tac_Toe
+      <Board />
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6],
+  ];
+
+  for (let line of lines) {
+    const [a,b,c] = line;
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] ===squares[c]) {
+      return squares[a];
+    }
+  }
+return null;
+}
